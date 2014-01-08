903f6ca45183cb0ec3daef2dd81cd727c874f2bd
Grigori Goronzy <greg@chown.ath.cx>
2014-01-08 15:30:23 +0100

    Add and use MCU model database

--- doc/dump-mcu.py
+++ doc/dump-mcu.py
@@ -19,14 +19,19 @@ for i in range(MCU_TABLE_SIZE):
     inp.seek(mcu_record_offset)
     mcu_record = inp.read(MCU_RECORD_SIZE)
     flags, name_ptr, mcu_id, code_size, ee_size, _, total_size, _ = struct.unpack("<8I", mcu_record)
+    mcu_id &= 0xffff
     
     mcu_name_offset = MCU_NAMES_OFFSET + (name_ptr - MCU_NAMES_PTR_OFFSET)
     inp.seek(mcu_name_offset)
     name_str = inp.read(16).split(b'\00')[0].decode("ascii")
 
-    # 1 KB are reserved for some reason. in some cases even more. very odd.
-    if ee_size > 0 and not name_str.startswith("IAP"):
-        ee_size -= 1024
+    # XXX: 1 KB are reserved one *some* MCUs for some reason
+    #if ee_size > 0 and not name_str.startswith("IAP"):
+    #    ee_size -= 1024
+
+    # STC12C54xx always have 12 KB eeprom
+    if name_str.startswith("STC12C54"):
+        ee_size = 12 * 1024
 
     print("MCUModel(name='%s', magic=0x%02x%02x, total=%d, code=%d, eeprom=%d)," %
         (name_str, mcu_id >> 8, mcu_id & 0xff, total_size, code_size, ee_size))
