8d782bb626e5363576ff39960aa78efb1e47d2af
Grigori Goronzy <greg@chown.ath.cx>
2014-01-15 01:09:29 +0100

    Add mail address to license text

--- stcgal.py
+++ stcgal.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# Copyright (c) 2013 Grigori Goronzy
+# Copyright (c) 2013 Grigori Goronzy <greg@chown.ath.cx>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
