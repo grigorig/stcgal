af35a6ba51eab5cb560bad86f2954b28e509b3a8
Grigori Goronzy <greg@chown.ath.cx>
2015-11-23 21:39:08 +0100

    setup: add metadata

--- setup.py
+++ setup.py
@@ -12,5 +12,23 @@ setup(
         "console_scripts": [
             "stcgal = stcgal.frontend:cli",
         ],
-    }
+    },
+    description = "STC MCU ISP flash tool",
+    url = "https://github.com/grigorig/stcgal",
+    author = "Grigori Goronzy",
+    author_email = "greg@kinoho.net",
+    license = "MIT License",
+    platforms = "any",
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Embedded Systems",
+        "Topic :: Software Development",
+    ],
 )
